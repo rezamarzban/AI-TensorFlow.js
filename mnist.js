a0dd33ffe137a2f5658956a5bcb73cbb648d818a
marzban2030 <73051714+marzban2030@users.noreply.github.com>
2022-07-10 02:28:23 +0430

    Add files via upload

---
+++ mnist.js
@@ -0,0 +1,82 @@
+
+const IMAGE_SIZE = 784;
+const NUM_CLASSES = 10;
+const NUM_DATASET_ELEMENTS = 65000;
+
+const NUM_TRAIN_ELEMENTS = 55000;
+const NUM_TEST_ELEMENTS = NUM_DATASET_ELEMENTS - NUM_TRAIN_ELEMENTS;
+
+const MNIST_IMAGES_SPRITE_PATH =
+    './mnist_images.png';
+const MNIST_LABELS_PATH =
+    './mnist_labels_uint8';
+
+/**
+ * A class that fetches the sprited MNIST dataset and returns ...
+ *
+ * NOTE: This will get much easier. For now, we do data fetching and
+ * manipulation manually.
+ */
+
+export class MnistData {
+  constructor() {
+  }
+
+  async load() {
+    // Make a request for the MNIST sprited image.
+    const img = new Image();
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d');
+    const imgRequest = new Promise((resolve, reject) => {
+      img.crossOrigin = '';
+      img.onload = () => {
+        img.width = img.naturalWidth;
+        img.height = img.naturalHeight;
+
+        const datasetBytesBuffer =
+            new ArrayBuffer(NUM_DATASET_ELEMENTS * IMAGE_SIZE * 4);
+
+        const chunkSize = 5000;
+        canvas.width = img.width;
+        canvas.height = chunkSize;
+
+        for (let i = 0; i < NUM_DATASET_ELEMENTS / chunkSize; i++) {
+          const datasetBytesView = new Float32Array(
+              datasetBytesBuffer, i * IMAGE_SIZE * chunkSize * 4,
+              IMAGE_SIZE * chunkSize);
+          ctx.drawImage(
+              img, 0, i * chunkSize, img.width, chunkSize, 0, 0, img.width,
+              chunkSize);
+
+          const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+          for (let j = 0; j < imageData.data.length / 4; j++) {
+            // All channels hold an equal value since the image is grayscale, so
+            // just read the red channel.
+            datasetBytesView[j] = imageData.data[j * 4] / 255;
+          }
+        }
+        this.datasetImages = new Float32Array(datasetBytesBuffer);
+
+        resolve();
+      };
+      img.src = MNIST_IMAGES_SPRITE_PATH;
+    });
+
+    const labelsRequest = fetch(MNIST_LABELS_PATH);
+    const [imgResponse, labelsResponse] =
+        await Promise.all([imgRequest, labelsRequest]);
+
+    this.datasetLabels = new Uint8Array(await labelsResponse.arrayBuffer());
+
+    // Slice the the images and labels into train and test sets.
+    this.trainImages =
+        this.datasetImages.slice(0, IMAGE_SIZE * NUM_TRAIN_ELEMENTS);
+    this.testImages = this.datasetImages.slice(IMAGE_SIZE * NUM_TRAIN_ELEMENTS);
+    this.trainLabels =
+        this.datasetLabels.slice(0, NUM_CLASSES * NUM_TRAIN_ELEMENTS);
+    this.testLabels =
+        this.datasetLabels.slice(NUM_CLASSES * NUM_TRAIN_ELEMENTS);
+  }
+
+}
---
+++ model.js
@@ -0,0 +1,22 @@
+import {MnistData} from './mnist.js';
+
+const data = new MnistData();
+
+data.load().then(
+    (onResolved) => {
+        const Xtrain = tf.tensor2d(data.trainImages, [55000, 784]);
+        const Ytrain = tf.tensor2d(data.trainLabels, [55000, 10]);
+        const Xtest = tf.tensor2d(data.testImages, [10000, 784]);
+        const Ytest = tf.tensor2d(data.testLabels, [10000, 10]);
+        const model = tf.sequential();
+        model.add(tf.layers.dense({inputShape: [784], units: 32, activation: 'relu'}));
+        model.add(tf.layers.dense({units: 10, activation: 'softmax'}));
+        model.summary();
+        model.compile({optimizer: 'adam', loss: 'categoricalCrossentropy', metrics: ['accuracy'], });
+        model.fit(Xtrain, Ytrain, {epochs: 1});
+      
+    },
+    (onRejected) => {
+        console.log("mnist data load failed.")
+    }
+)
