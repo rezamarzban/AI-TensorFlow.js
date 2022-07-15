3828fe906f2a63b787cedb9c8d10e51e8088191a
marzban2030 <73051714+marzban2030@users.noreply.github.com>
2022-07-15 12:29:48 +0430

    Update model.js

--- model.js
+++ model.js
@@ -13,6 +13,8 @@ data.load().then(
         model.add(tf.layers.dense({units: 10, activation: 'softmax'}));
         model.summary();
         model.compile({optimizer: 'adam', loss: 'categoricalCrossentropy', metrics: ['accuracy'], });
+        //tensorflow.js model.fit() doesn't work, Search it in Google.
+        //This line of code do nothing, I think this is because of some problems due to tensorflow.js, RAM and GPU hardware resource limits.
         model.fit(Xtrain, Ytrain, {epochs: 10}).then(() => {
         model.evaluate(Xtrain, Ytrain);
         });
