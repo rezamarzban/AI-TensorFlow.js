cbd9030422751465a294c9a77b6137765187a6d8
marzban2030 <73051714+marzban2030@users.noreply.github.com>
2022-09-07 10:32:34 +0430

    Update mnist.js

--- mnist.js
+++ mnist.js
@@ -7,7 +7,7 @@ const NUM_TRAIN_ELEMENTS = 55000;
 const NUM_TEST_ELEMENTS = NUM_DATASET_ELEMENTS - NUM_TRAIN_ELEMENTS;
 
 const MNIST_IMAGES_SPRITE_PATH =
-    './mnist_images.png';
+    'https://storage.googleapis.com/learnjs-data/model-builder/mnist_images.png';
 const MNIST_LABELS_PATH =
     'https://storage.googleapis.com/learnjs-data/model-builder/mnist_labels_uint8';
 
